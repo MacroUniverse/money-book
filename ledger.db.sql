fe30cc24aaeb9b67afb4fc1bbae7dcb8c322c36d
MacroUniverse <270174408@qq.com>
2026-02-09 18:19:17 +0800

    1

---
+++ ledger.db.sql
@@ -0,0 +1,91 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"name"	TEXT NOT NULL UNIQUE,
+	"currency"	TEXT,
+	PRIMARY KEY("name")
+);
+INSERT INTO accounts VALUES('BTC','btc');
+INSERT INTO accounts VALUES('ETH','eth');
+INSERT INTO accounts VALUES('apple card','usd');
+INSERT INTO accounts VALUES('chase credit','usd');
+INSERT INTO accounts VALUES('chase debit','usd');
+INSERT INTO accounts VALUES('commerce bank','usd');
+INSERT INTO accounts VALUES('discover','usd');
+INSERT INTO accounts VALUES('paypal','usd');
+INSERT INTO accounts VALUES('中信','rmb');
+INSERT INTO accounts VALUES('工行','rmb');
+INSERT INTO accounts VALUES('建行','rmb');
+INSERT INTO accounts VALUES('微信','rmb');
+INSERT INTO accounts VALUES('微信小','rmb');
+INSERT INTO accounts VALUES('支付宝','rmb');
+CREATE TABLE IF NOT EXISTS "main" (
+	"ID"	INTEGER NOT NULL UNIQUE,
+	"日期"	TEXT,
+	"描述"	TEXT,
+	"RMB"	TEXT,
+	"USD"	TEXT,
+	"对方"	TEXT,
+	"账户"	TEXT,
+	"备注"	TEXT,
+	"备忘"	TEXT,
+	"标签1"	TEXT,
+	"标签2"	TEXT,
+	"标签3"	TEXT,
+	"关联1"	INTEGER,
+	FOREIGN KEY("账户") REFERENCES "accounts"("name"),
+	FOREIGN KEY("标签1") REFERENCES "tags"("名称"),
+	FOREIGN KEY("标签2") REFERENCES "tags"("名称"),
+	FOREIGN KEY("对方") REFERENCES "targets"("名称"),
+	FOREIGN KEY("标签3") REFERENCES "tags"("名称"),
+	FOREIGN KEY("关联1") REFERENCES "main"("ID"),
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+INSERT INTO main VALUES(1,'240513','换 $1383.67','-10000',NULL,'kush','微信',NULL,NULL,'换汇',NULL,NULL,23);
+INSERT INTO main VALUES(10,'240401','4 月亲情卡','1838.14',NULL,'宝贝','支付宝',NULL,NULL,'送宝贝',NULL,NULL,NULL);
+INSERT INTO main VALUES(100,'240531','Lyft 打车',NULL,'9.8',NULL,'paypal',NULL,NULL,'生活',NULL,NULL,NULL);
+INSERT INTO main VALUES(1000,'260111','北京龙泉驾校（手动挡补差价）','400',NULL,NULL,'微信',NULL,NULL,'车',NULL,NULL,NULL);
+INSERT INTO main VALUES(1002,'260118','请宝贝吃 Papa Danilo 意大利餐厅 190+76','266',NULL,NULL,'微信',NULL,NULL,'送宝贝',NULL,NULL,NULL);
+INSERT INTO main VALUES(995,'260101','1 月生活杂费（支付宝）','210.79',NULL,NULL,'支付宝','37.32+34.8+12+3+51+20+20+14.67+3+15',NULL,'生活',NULL,NULL,NULL);
+INSERT INTO main VALUES(996,'260101','1 月正常饮食（微信）','123.88',NULL,NULL,'微信','18.98+26.4+18.8+19.9+17.9+21.9',NULL,'吃喝',NULL,NULL,NULL);
+INSERT INTO main VALUES(997,'260101','1 月生活杂费（微信）','78.6',NULL,NULL,'微信','16.8+39+16.8+6',NULL,'生活',NULL,NULL,NULL);
+INSERT INTO main VALUES(999,'251130','GitKraken 一年会员',NULL,'25.44',NULL,'discover',NULL,NULL,'软件',NULL,NULL,NULL);
+CREATE TABLE IF NOT EXISTS "tags" (
+	"名称"	TEXT NOT NULL UNIQUE,
+	"说明"	TEXT,
+	PRIMARY KEY("名称")
+);
+INSERT INTO tags VALUES('安置',NULL);
+INSERT INTO tags VALUES('亲人',NULL);
+INSERT INTO tags VALUES('还钱',NULL);
+INSERT INTO tags VALUES('退款',NULL);
+INSERT INTO tags VALUES('送宝贝',NULL);
+CREATE TABLE IF NOT EXISTS "target_tags" (
+	"名称"	TEXT NOT NULL UNIQUE,
+	"说明"	TEXT,
+	PRIMARY KEY("名称")
+);
+INSERT INTO target_tags VALUES('亲人',NULL);
+INSERT INTO target_tags VALUES('修车',NULL);
+INSERT INTO target_tags VALUES('超市',NULL);
+INSERT INTO target_tags VALUES('餐饮',NULL);
+CREATE TABLE IF NOT EXISTS "targets" (
+	"名称"	TEXT NOT NULL UNIQUE,
+	"别名"	TEXT,
+	"备注"	TEXT,
+	"联系方式"	TEXT,
+	"tag1"	TEXT,
+	"tag2"	TEXT,
+	FOREIGN KEY("tag1") REFERENCES "target_tags"("名称"),
+	FOREIGN KEY("tag2") REFERENCES "target_tags"("名称"),
+	PRIMARY KEY("名称")
+);
+INSERT INTO targets VALUES('IRS',NULL,'退税',NULL,NULL,NULL);
+INSERT INTO targets VALUES('淘宝',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO targets VALUES('爱一帆','','盗版电影','',NULL,NULL);
+INSERT INTO targets VALUES('爸爸',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO targets VALUES('闲鱼',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO targets VALUES('麦当劳',NULL,NULL,NULL,NULL,NULL);
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('main',1034);
+COMMIT;
---
+++ prompt.md
@@ -0,0 +1,7 @@
+Write a personal financial management app called `money-book`
+- with React and JS and node.js that
+- can run locally on my Ubuntu, Mac, and Windows system.
+- use SQLite3 as db, and an example dump file `ledger.db.sql` (the app should not change anything except records)
+- there should be a friendly statistics function for a month, or a selected period of time
+- user should be able to add new records undo changes to all tables
+- as you go, write a developer manual so that other developers can join
